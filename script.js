ec290f1b4c15b8187ecb7a62fa6d2282d73dc9a5
tulasisagar <tulasisagar.devops@gmail.com>
2026-06-26 18:04:12 +0000

    Completed portfolio homepage structure and styling

--- script.js
+++ script.js
@@ -1,1 +1,50 @@
-console.log("Portfolio Loaded Successfully 🚀");
+const roles = [
+    "DevOps Engineer",
+    "AWS Cloud Enthusiast",
+    "Linux Automation Learner",
+    "CI/CD Builder"
+];
+
+let roleIndex = 0;
+let charIndex = 0;
+let deleting = false;
+
+const typing = document.getElementById("typing");
+
+function type() {
+
+    const current = roles[roleIndex];
+
+    if (!deleting) {
+
+        typing.textContent = current.substring(0, charIndex++);
+
+        if (charIndex > current.length) {
+
+            deleting = true;
+
+            setTimeout(type, 1200);
+
+            return;
+
+        }
+
+    } else {
+
+        typing.textContent = current.substring(0, charIndex--);
+
+        if (charIndex < 0) {
+
+            deleting = false;
+
+            roleIndex = (roleIndex + 1) % roles.length;
+
+        }
+
+    }
+
+    setTimeout(type, deleting ? 40 : 100);
+
+}
+
+type();
