ebc3c2093931c27b6ae3011d21abe0e57b1015a9
tulasisagar <tulasisagar.devops@gmail.com>
2026-06-27 05:46:40 +0000

    Added resume and animations

--- script.js
+++ script.js
@@ -48,3 +48,30 @@ function type() {
 }
 
 type();
+const topBtn = document.getElementById("topBtn");
+
+window.onscroll = function () {
+
+    if(document.body.scrollTop > 300 || document.documentElement.scrollTop > 300){
+
+        topBtn.style.display="block";
+
+    }else{
+
+        topBtn.style.display="none";
+
+    }
+
+}
+
+topBtn.onclick=function(){
+
+    window.scrollTo({
+
+        top:0,
+
+        behavior:"smooth"
+
+    });
+
+}
